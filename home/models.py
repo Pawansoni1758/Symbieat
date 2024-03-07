1f1539b7a770efde301a51b30d609b23cb9c5985
Pawan Kumar <pawan2004soni@gmail.com>
2024-03-07 15:30:38 +0530

    enable filter buttons

--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('BR', 'Burger'),
+    ('MG', 'Maggie'),
+    ('PST', 'Pasta'),
+    ('BVG', 'Beverages'),
+    ('SDH', 'Sandwitch')
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=3)
+    Product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, HttpResponse
-
+from django.views import View
+from .models import *
+from django.db.models import Count
 # Create your views here.
 
 
 def home(request):
     return render(request, 'app/index.html')
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, 'app/category.html', locals())
