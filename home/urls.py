0dd3fc1f67741141fc7f335a0b69921b2ca2c3e0
Pawan Kumar <pawan2004soni@gmail.com>
2024-03-10 14:32:06 +0530

    change password and logout

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordResetForm
+from .forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
 
 urlpatterns = [
     path("",views.home, name="home"),
@@ -19,5 +19,10 @@ urlpatterns = [
         form_class=MyPasswordResetForm), name='password_reset'),
         path('profile/', views.ProfileView.as_view(), name='profile'),
         path('address/', views.address, name='address'),
-        path('updateAddress/<int:pk>', views.updateAddress.as_view(), name='updateAddress')
+        path('updateAddress/<int:pk>', views.updateAddress.as_view(), name='updateAddress'),
+        path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html',
+            form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
+        path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name=
+            'app/passwordchangedone.html'), name='passwordchangedone'),
+        path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout')
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
